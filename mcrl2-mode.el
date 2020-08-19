067b77471f7753293e4e9d073c25a2f0939a008b
Casper Bach Poulsen <casperbp@gmail.com>
2020-08-20 00:49:55 +0200

    Improved output buffer interaction

--- mcrl2-mode.el
+++ mcrl2-mode.el
@@ -75,7 +75,8 @@
     (use-local-map mcrl2-mode-map)
     (run-hooks 'mcrl2-mode-hook)
     (local-set-key (kbd "<backtab>") 'mcrl2-unindent)
-    (setq-local tab-width 2)))
+    (setq-local tab-width 2)
+    (setq-local mcrl2-output-buffer nil)))
 
 (define-derived-mode mcf-mode prog-mode "mCRL2 mode"
   "Major mode for editing mcrl2 and mcf files."
@@ -95,71 +96,89 @@
 
 (provide 'mcrl2-mode)
 
+(defun mcrl2-get-output-buffer ()
+  (if mcrl2-output-buffer
+      (if (process-live-p (get-buffer-process mcrl2-output-buffer))
+          (if (y-or-n-p "A process is running in the associated output buffer. Create new associated output buffer?")
+              (setq mcrl2-output-buffer (generate-new-buffer "*mCRL2 Output*"))
+            nil)
+        mcrl2-output-buffer)
+    (setq mcrl2-output-buffer (generate-new-buffer "*mCRL2 Output*"))))
+
+(defun mcrl2-cmd (cmd)
+  (let ((out (mcrl2-get-output-buffer)))
+    (if out
+        (async-shell-command cmd out)
+      nil)))
+        
 
 (defun mcrl2-create-lps-reg (&optional set-line)
   (interactive)
-  (async-shell-command (concat "mcrl22lps -l regular "
-                               (shell-quote-argument buffer-file-name)
-                               " > "
-                               (shell-quote-argument (concat buffer-file-name
-                                                             ".lps")))))
+  (mcrl2-cmd (concat "mcrl22lps -l regular "
+                     (shell-quote-argument buffer-file-name)
+                     " > "
+                     (shell-quote-argument (concat buffer-file-name
+                                                   ".lps")))))
 
 (defun mcrl2-create-lps-reg2 (&optional set-line)
   (interactive)
-  (async-shell-command (concat "mcrl22lps -l regular2 "
-                               (shell-quote-argument buffer-file-name) " > "
-                               (shell-quote-argument (concat buffer-file-name ".lps")))))
+  (mcrl2-cmd (concat "mcrl22lps -l regular2 "
+                     (shell-quote-argument buffer-file-name) " > "
+                     (shell-quote-argument (concat buffer-file-name ".lps")))))
 
 (defun mcrl2-create-lps-stack (&optional set-line)
   (interactive)
-  (async-shell-command (concat "mcrl22lps -l stack "
-                               (shell-quote-argument buffer-file-name) " > "
-                               (shell-quote-argument (concat buffer-file-name ".lps")))))
+  (mcrl2-cmd (concat "mcrl22lps -l stack "
+                     (shell-quote-argument buffer-file-name) " > "
+                     (shell-quote-argument (concat buffer-file-name ".lps")))))
 
  ;; LTS Creation Functions
 (defun mcrl2-create-lts (&optional set-line)
   (interactive)
-  (async-shell-command (concat "lps2lts --verbose "
-                               (shell-quote-argument (concat buffer-file-name ".lps"))
-                               " "
-                               (shell-quote-argument buffer-file-name) ".lts" )))
+  (mcrl2-cmd (concat "lps2lts --verbose "
+                     (shell-quote-argument (concat buffer-file-name ".lps"))
+                     " "
+                     (shell-quote-argument buffer-file-name) ".lts" )))
 
 ;; PBES Creation Functions
 (defun mcf-create-lps-pbes (&optional set-line)
   (interactive)
-  (async-shell-command (concat "lps2pbes -c "
-                               (read-file-name "Enter .lps file name:")
-                               " -f " (shell-quote-argument buffer-file-name)
-                               " " (shell-quote-argument (concat buffer-file-name ".pbes")) )))
+  (mcrl2-cmd (concat "lps2pbes -c "
+                     (read-file-name "Enter .lps file name:")
+                     " -f " (shell-quote-argument buffer-file-name)
+                     " " (shell-quote-argument (concat buffer-file-name ".pbes")) )))
 
 (defun mcf-pbes-bool (&optional set-line)
   (interactive)
-  (async-shell-command (concat "pbes2bool --verbose "
-                               (shell-quote-argument buffer-file-name) ".pbes")))
+  (mcrl2-cmd (concat "pbes2bool --verbose "
+                     (shell-quote-argument buffer-file-name) ".pbes")))
 
 ;; PBES Model Checking Functions
 (defun mcrl2-check-pbes-lps (&optional set-line)
   (interactive)
-  (async-shell-command (concat "pbessolve -v --file="
-                               (shell-quote-argument (concat buffer-file-name ".lps"))
-                               " "
-                               (shell-quote-argument (concat buffer-file-name ".pbes")) )))
+  (mcrl2-cmd (concat "pbessolve -v --file="
+                     (shell-quote-argument (concat buffer-file-name ".lps"))
+                     " "
+                     (shell-quote-argument (concat buffer-file-name ".pbes")) )))
 (defun mcrl2-check-pbes-lts (&optional set-line)
-  (interactive)
-  (async-shell-command (concat "pbessolve -v --file="
-                               (shell-quote-argument (concat buffer-file-name ".lts"))
-                               " "
-                               (shell-quote-argument (concat buffer-file-name ".pbes")) )))
+  (mcrl2-cmd (concat "pbessolve -v --file="
+                     (shell-quote-argument (concat buffer-file-name ".lts"))
+                     " "
+                     (shell-quote-argument (concat buffer-file-name ".pbes")) )))
 
 ;; LTS Graph Functions
 (defun mcrl2-lts-graph-current (&optional set-line)
   (interactive)
-  (async-shell-command (concat "ltsgraph " (shell-quote-argument buffer-file-name) ".lts")))
+  (mcrl2-cmd (concat "ltsgraph " (shell-quote-argument buffer-file-name) ".lts")))
 
 (defun mcrl2-lts-graph-evidence (&optional set-line)
   (interactive)
-  (async-shell-command (concat "ltsgraph "  (shell-quote-argument buffer-file-name) ".pbes.evidence.lts")))
+  (mcrl2-cmd (concat "ltsgraph "
+                     (shell-quote-argument buffer-file-name)
+                     ".pbes.evidence.lts")))
 
 (defun mcrl2-lts-sim-current (&optional set-line)
   (interactive)
-  (async-shell-command (concat "lpssim " (shell-quote-argument buffer-file-name) ".lps")))
+  (mcrl2-cmd (concat "lpssim "
+                     (shell-quote-argument buffer-file-name)
+                     ".lps")))
