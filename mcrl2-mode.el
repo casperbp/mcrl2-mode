caa1d2910e508091648b88a70ca40c9d3339ac01
Casper Bach Poulsen <casperbp@gmail.com>
2020-08-21 11:32:28 +0200

    more keywords

--- mcrl2-mode.el
+++ mcrl2-mode.el
@@ -26,7 +26,7 @@
                            "sum" "eqn" "map" "var"
                            "in" "mu" "nu" "forall" "exists"))
              (x-types '("Bool" "Nat" "Int" "Set" "List" "Real" "Pos"))
-             (x-functions '("allow" "comm" "hide"))
+             (x-functions '("allow" "comm" "hide" "rename" "block"))
 
              (x-keywords-regexp (regexp-opt x-keywords 'symbols))
              (x-types-regexp (regexp-opt x-types 'symbols))
@@ -34,7 +34,7 @@
              )
         `(
           (,x-keywords-regexp . font-lock-keyword-face)
-          (, "\\(true\\|false\\|lambda\\|min\\|max\\|succ\\|pred\\|div\\|mod\\|floor\\|ceil\\|abs\\|exp\\|round\\|Pos2Nat\\|Nat2Pos\\|tau\\|delta\\)[^_]" (1 font-lock-keyword-face))
+          (, "\\(true\\|false\\|lambda\\|min\\|max\\|succ\\|pred\\|div\\|mod\\|floor\\|ceil\\|abs\\|exp\\|round\\|Pos2Nat\\|Nat2Pos\\|tau\\|delta\\|in\\)[^_]" (1 font-lock-keyword-face))
           (,x-types-regexp . font-lock-type-face)
           (,x-functions-regexp . font-lock-function-name-face)
           (";\\|:\\|\\.\\|,\\|=>\\|=\\|+\\|->\\|-\\|*\\|\|\\|!\\|#\\|\<\>\\|(\\|)\\|{\\|}\\|\\[\\|\\]\\|<\\|>\\|&&\\|&" . font-lock-constant-face)
